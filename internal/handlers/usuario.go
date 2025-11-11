fec1acf0cade0cb89346f6ee9e0fb2e13978c848
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-11-10 21:54:56 -0300

    feat: finalizando handler do usuario

--- internal/handlers/usuario.go
+++ internal/handlers/usuario.go
@@ -5,6 +5,9 @@ import (
 	"go-crud/internal/model"
 	"go-crud/internal/service"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // UsuarioHandler e o handler de usuario
@@ -32,4 +35,81 @@ func (h *UsuarioHandler) CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"mensagem": "Usuário criado"})
+}
+
+// AtualizarUsario atualiza um usuario
+func (h *UsuarioHandler) AtualizarUsario(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["idUsuario"]
+
+	ID, erro := strconv.ParseUint(idStr, 10, 64)
+	if erro != nil {
+		http.Error(w, "ID do usuario inválido"+erro.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var u model.Usuario
+	if erro := json.NewDecoder(r.Body).Decode(&u); erro != nil {
+		http.Error(w, "JSON inválido: "+erro.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if erro := h.Service.AtualizarUsuario(uint(ID), u); erro != nil {
+		http.Error(w, "Erro ao atualizar usuário: "+erro.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// DeletarUsuario deleta um usuario
+func (h *UsuarioHandler) DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["idUsuario"]
+
+	ID, erro := strconv.ParseUint(idStr, 10, 64)
+	if erro != nil {
+		http.Error(w, "ID do usuario inválido"+erro.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if erro := h.Service.DeleteUsuario(uint(ID)); erro != nil {
+		http.Error(w, "Erro ao deletar usuário: "+erro.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// GetUsuario pega o usuario a partir do id dele
+func (h *UsuarioHandler) GetUsuario(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["idUsuario"]
+
+	ID, erro := strconv.ParseUint(idStr, 10, 64)
+	if erro != nil {
+		http.Error(w, "ID do usuario inválido"+erro.Error(), http.StatusBadRequest)
+		return
+	}
+
+	usuario, erro := h.Service.GetUsuario(uint(ID))
+	if erro != nil {
+		http.Error(w, "Erro ao deletar usuário: "+erro.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	json.NewEncoder(w).Encode(map[string]model.Usuario{"mensagem": usuario})
+}
+
+// GetUsuarios pega todos os usuarios do banco de dados
+func (h *UsuarioHandler) GetUsuarios(w http.ResponseWriter, r *http.Request) {
+	usuarios, erro := h.Service.GetUsuarios()
+	if erro != nil {
+		http.Error(w, "Erro ao deletar usuário: "+erro.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	json.NewEncoder(w).Encode(map[string][]model.Usuario{"mensagem": usuarios})
 }
