de56203f8d8899f203ee518b07d4bf6074879add
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-11-10 21:21:58 -0300

    feat: finalizando crud de usuario

--- internal/repository/usuario.go
+++ internal/repository/usuario.go
@@ -26,3 +26,63 @@ func (r *UsuarioRepository) Create(u model.Usuario) error {
 	_, erro = stmt.Exec(u.Nome, u.Email, u.Senha)
 	return erro
 }
+
+// Update vai atualizar um usuario
+func (r *UsuarioRepository) Update(idUsuario uint32, u model.Usuario) error {
+	stmt, erro := r.DB.Prepare("UPDATE usuario SET nome = ?, email = ?, senha = ? WHERE idusuario = ?")
+	if erro != nil {
+		return erro
+	}
+	defer stmt.Close()
+
+	_, erro = stmt.Exec(u.Nome, u.Email, u.Senha, idUsuario)
+	return erro
+}
+
+// Delete vai deletar um usuario
+func (r *UsuarioRepository) Delete(idUsuario uint32) error {
+	stmt, erro := r.DB.Prepare("DELETE FROM usuario WHERE idusuario = ?")
+	if erro != nil {
+		return erro
+	}
+	defer stmt.Close()
+
+	_, erro = stmt.Exec(idUsuario)
+	return erro
+}
+
+// GetUsuarios vai trazer todos os usaurios
+func (r *UsuarioRepository) GetUsuarios() ([]model.Usuario, error) {
+	rows, erro := r.DB.Query("SELECT idusuario, nome, email, senha FROM usuario")
+	if erro != nil {
+		return nil, erro
+	}
+	defer rows.Close()
+
+	var usuarios []model.Usuario
+
+	for rows.Next() {
+		var u model.Usuario
+
+		if erro := rows.Scan(&u.ID, &u.Nome, &u.Email, &u.Senha); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, u)
+	}
+
+	return usuarios, nil
+}
+
+// GetUsuario vai trazer apenas o usuario com o id que foi passado como parametro
+func (r *UsuarioRepository) GetUsuario(idUsuario uint32) (model.Usuario, error) {
+	row := r.DB.QueryRow("SELECT idusuario, nome, email, senha FROM usuario WHERE idusuario = ?", idUsuario)
+
+	var u model.Usuario
+
+	if erro := row.Scan(&u.ID, &u.Nome, &u.Email, &u.Senha); erro != nil {
+		return model.Usuario{}, erro
+	}
+
+	return u, nil
+}
