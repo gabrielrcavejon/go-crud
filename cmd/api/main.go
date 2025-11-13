3b6ac1a22d0542641b58bb87dd2041bcfd9cef83
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-11-13 20:23:44 -0300

    feat: alterado as rotas para poderem ser acessadas apenas com token

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,9 +9,18 @@ import (
 	"go-crud/internal/service"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Carrega o .ENV
+	erro := godotenv.Load("../../.env")
+	if erro != nil {
+		log.Fatal(".env não encontrado")
+	}
+
+	// Conecta no banco
 	database.Conectar()
 
 	// Faz a injeção de dependencia automatica
@@ -30,8 +39,10 @@ func main() {
 	loginService := service.NewLoginService(usuarioRepo)
 	loginHandler := handlers.NewLoginHandler(loginService)
 
+	// Configura as rotas da api
 	router := routes.Setup(usuarioHandler, produtoHandler, loginHandler)
 
+	// Start na api
 	fmt.Println("API em http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
